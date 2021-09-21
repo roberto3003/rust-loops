4d000ec87f6dab2bc8034d8b5aeaabbb90713050
bobby3003 <roberto3003@riseup.net>
2021-09-21 17:46:10 +0700

    loops

--- src/main.rs
+++ src/main.rs
@@ -23,4 +23,25 @@ fn main() {
     for element in b.iter() {
         println!("the value is: {}", element);
     }
+
+    let mut i: i8 = 1;
+    let something = loop {
+        i *= 2;
+        if i > 100 {
+            break i;
+        }
+    };
+
+    assert_eq!(something, 127);
+
+    let mut counter: i8 = 0;
+
+    while counter < 10 {
+        println!("hello!");
+        counter = counter + 1;
+    }
+
+    for item in 0..=5 {
+        println!("{}", item * 2);
+    }
 }
